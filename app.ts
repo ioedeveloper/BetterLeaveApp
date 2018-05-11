94b4757bffe61b74451f30d2f710406700f802ef
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-05-11 14:52:53 +0100

    Established connection with mongodb

--- app.ts
+++ app.ts
@@ -1,1 +1,8 @@
-console.log("Application Starting...");
+import {createConnection} from "typeorm";
+
+// connection settings are in the "ormconfig.json" file
+
+// create app db connection.
+createConnection().then(async connection => {
+    console.log("Database Connection Created");
+}).catch(error => console.log("Data Access Error : ", error));
