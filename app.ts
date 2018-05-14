13e3797c49da49eb6a22ae54586323ee3b343e03
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-05-14 15:51:50 +0100

    Bootstrap database connection on app startup

--- app.ts
+++ app.ts
@@ -3,5 +3,9 @@ import bodyParser = require("body-parser");
 import path = require("path");
 import * as apiCtrl from "./controllers/apiController";
 import { apiRoutes } from "./routes/api";
+import {createConnection, Connection} from "typeorm";
 
+createConnection().then(async connection (connection) => {
+
+});
 // create app db connection
