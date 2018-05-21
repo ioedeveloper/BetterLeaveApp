161cdb795f6325d81f75fe769e4270af5abf2345
ioedeveloper <ioedeveloper@DESKTOP-113GA6A>
2018-05-21 16:22:40 +0100

    Uncle Joe

--- api_models/organisationApiModel.ts
+++ api_models/organisationApiModel.ts
@@ -3,9 +3,3 @@
 /**
  * Handles the shape of the organisation api
  */
-export class OrganisationApiModel {
-    // tslint:disable-next-line:max-line-length
-    constructor(public organizationName: string, public organizationAddress: string, public organizationDescription?: string, public admin:Staff) {
-
-    }
-}
